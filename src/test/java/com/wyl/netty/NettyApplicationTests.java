6c375bdebfe6d0e7042ad2e9de448125c51ee77e
small-calf <1833415795@qq.com>
2021-06-13 16:14:07 +0800

    learning

---
+++ src/test/java/com/wyl/netty/NettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyl.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
